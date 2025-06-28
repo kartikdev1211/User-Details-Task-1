6e6450119232ed9314dfe2f92ddc1fc3e8352460
kartikdev1211 <kartikkysp6750@gmail.com>
2025-06-28 12:52:51 +0530

    api works fine now

--- lib/repo/user_repo.dart
+++ lib/repo/user_repo.dart
@@ -1,18 +1,40 @@
 import 'dart:convert';
 import 'dart:developer';
+import 'dart:io';
 import 'package:user_details/models/user_model.dart';
-import 'package:http/http.dart'as http;
-class UserRepo{
-  Future<List<User>> fetchUser()async{
-    final url="https://jsonplaceholder.typicode.com/users";
-    final response=await http.get(Uri.parse(url));
-    log("Response is: ${response.body}");
-    if(response.statusCode==200){
-      final List user=jsonDecode(response.body);
-      log("Users are: $user");
-      return user.map((j)=>User.fromJson(j)).toList();
-    }else{
-      throw Exception("API Failed");
+import 'package:http/http.dart' as http;
+
+class UserRepo {
+  static const endpoint = 'https://jsonplaceholder.typicode.com/users';
+  Future<List<User>> fetchUser() async {
+    try {
+      final uri = Uri.parse(endpoint);
+      final response = await http.get(
+        uri,
+        headers: {
+          'User-Agent': 'Mozilla/5.0(Flutter; dart.io)',
+          'Accpet': 'application/json',
+        },
+      );
+      log("status is: ${response.statusCode}");
+      log("Body is: ${response.body}");
+      if (response.statusCode == 200) {
+        final List<dynamic> decoded = jsonDecode(response.body);
+        return decoded
+            .map((e) => User.fromJson(e as Map<String, dynamic>))
+            .toList();
+      } else {
+        throw HttpException(
+          "CloudFare/WAF blocked request (status ${response.statusCode})",
+          uri: uri,
+        );
+      }
+    } on SocketException {
+      throw const HttpException("No internet connection");
+    } on FormatException {
+      throw const FormatException("Bad JSON format");
+    } catch (e) {
+      rethrow;
     }
   }
 }
--- lib/screens/user_screen.dart
+++ lib/screens/user_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:user_details/bloc/user_bloc.dart';
@@ -14,7 +13,7 @@ class UserScreen extends StatefulWidget {
 }
 
 class _UserScreenState extends State<UserScreen> {
-  final searchController=TextEditingController();
+  final searchController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,62 +24,90 @@ class _UserScreenState extends State<UserScreen> {
             hintText: "Search by name",
             border: OutlineInputBorder(),
           ),
-          onChanged: (query)=>context.read<UserBloc>().add(SearchUsers(searchQuery: query)),
+          onChanged: (query) =>
+              context.read<UserBloc>().add(SearchUsers(searchQuery: query)),
         ),
         actions: [
-          IconButton(onPressed: (){
-            searchController.clear();
-            context.read<UserBloc>().add(SearchUsers(searchQuery: ''));
-          }, icon: Icon(Icons.clear))
+          IconButton(
+            onPressed: () {
+              searchController.clear();
+              context.read<UserBloc>().add(SearchUsers(searchQuery: ''));
+            },
+            icon: Icon(Icons.clear),
+          ),
         ],
       ),
-      body: BlocBuilder<UserBloc,UserState>(builder: (context,state){
-        switch(state){
-          case UserLoading():
-            return const CircularProgressIndicator();
-          case UserError():
-            return Center(child: Text(state.error),);
-          case UserLoaded():
-            return _buildList(state.users);
-          default:
-            return const SizedBox.shrink();
-        }
-      }),
+      body: BlocBuilder<UserBloc, UserState>(
+        builder: (context, state) {
+          switch (state) {
+            case UserLoading():
+              return Center(child: const CircularProgressIndicator());
+            case UserError():
+              return Center(child: Text(state.error));
+            case UserLoaded():
+              return _buildList(state.users, context);
+            default:
+              return const SizedBox.shrink();
+          }
+        },
+      ),
     );
   }
-  Widget _buildList(List<User> user){
-    if(user.isEmpty){
-      return const Center(child:Text("No user found.",style:TextStyle(fontSize:16,color: Colors.grey)));
+
+  Widget _buildList(List<User> user, BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final horizontalPadding = screenWidth * 0.03; // 3% of screen width
+    final verticalPadding = screenWidth * 0.015; // 1.5% of screen width
+
+    if (user.isEmpty) {
+      return const Center(
+        child: Text(
+          "No user found.",
+          style: TextStyle(fontSize: 16, color: Colors.grey),
+        ),
+      );
     }
+
     return ListView.builder(
-      physics: BouncingScrollPhysics(),
+      physics: const BouncingScrollPhysics(),
       itemCount: user.length,
-      itemBuilder: (context,index){
-        final users=user[index];
+      itemBuilder: (context, index) {
+        final users = user[index];
         return Card(
-          margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+          margin: EdgeInsets.symmetric(
+            horizontal: horizontalPadding,
+            vertical: verticalPadding,
+          ),
           elevation: 2,
-          child: ListTile(
-            contentPadding: const EdgeInsets.all(12),
-            title: Text(
-              users.name,
-              style: const TextStyle(fontWeight: FontWeight.bold),
-            ),
-            subtitle: Column(
+          child: Padding(
+            padding: EdgeInsets.all(horizontalPadding),
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                Text(
+                  users.name,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.005),
+
                 Text('Username: ${users.username}'),
                 Text('Email: ${users.email}'),
                 Text('Phone: ${users.phone}'),
                 Text('Website: ${users.website}'),
-                const SizedBox(height: 4),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.0075),
+
                 Text(
-                  'Address: ${users.address.street}, ${users.address.suite}, ${users.address.city}, ${users.address.zipcode}',
+                  'Address: ${users.address.street}, ${users.address.suite}, '
+                  '${users.address.city}, ${users.address.zipcode}',
                 ),
                 Text(
                   'Geo: (${users.address.geo.lat}, ${users.address.geo.lng})',
                 ),
-                const SizedBox(height: 4),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.0075),
+
                 Text(
                   'Company: ${users.company.name} - ${users.company.catchPhrase}',
                 ),
@@ -88,6 +115,7 @@ class _UserScreenState extends State<UserScreen> {
             ),
           ),
         );
-      },);
+      },
+    );
   }
 }
